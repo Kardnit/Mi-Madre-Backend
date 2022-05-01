9f10e01e79c04a8e5f0c08c129690dd1e9e10e76
Kardnit <a.borgaiskender@gmail.com>
2022-05-01 17:47:01 +0200

    test

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-# define base docker image
-FROM openjdk:17-alpine
-LABEL maintainer="Borga"
-ADD target/MiMadre-0.0.1-SNAPSHOT.jar mimadre-docker.jar
-ENTRYPOINT ["java", "-jar", "mimadre-docker.jar"]
--- src/main/java/MiMadre/PatternDesign/PatternDesign.java
+++ src/main/java/MiMadre/PatternDesign/PatternDesign.java
@@ -1,5 +1,8 @@
 package MiMadre.PatternDesign;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
